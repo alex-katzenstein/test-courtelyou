bc34865234752e771f1b6bb1aa82196ef45abbf5
Alex Katzenstein <alex@bitbean.com>
2025-08-12 21:40:56 -0400

    feat: create work orders module with context, service, and types

---
+++ cortelyou-ui/src/work-orders/index.ts
@@ -0,0 +1,4 @@
+export { WorkOrdersProvider, useWorkOrders } from './WorkOrdersContext';
+export type { WorkOrder } from './WorkOrdersContext';
+export { default as WorkOrders } from './WorkOrders';
+export { default as workOrdersService } from './workOrdersService';
